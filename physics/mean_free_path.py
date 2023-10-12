695f5a5a3a1750ace0bb5c12723bf1350cf6853e
pluto-tofu <paarthqwerty@gmail.com>
2023-10-12 21:57:07 +0530

    started the file description

---
+++ physics/mean_free_path.py
@@ -0,0 +1,15 @@
+"""
+Title : Computing the mean free path for the molecules of a gas
+
+A gas molecule's mean free path λ is its average path length
+between collisions.
+
+Let's look at the motion of a gas molecule inside an ideal 
+gas; a typical molecule inside an ideal gas will abruptly 
+change its direction and speed as it collides elastically with 
+other molecules of the same gas. Though between the 
+collisions, the molecule will move in a straight line at some 
+constant speed, this is applicable for all the molecules in 
+the gas.
+
+"""
